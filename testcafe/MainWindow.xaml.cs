8d5d8888100e64af9ef9f2741760d5a05c7dd455
danrechi <danrechi@mail.ru>
2023-07-08 03:15:21 +0300

    second

---
+++ testcafe/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.SqlClient;
+using System.Windows;
+
+namespace Wpf_cafe
+{
+    public partial class MainWindow : Window
+    {
+        private const string ConnectionString = "Server=DESKTOP-PGFL9BA\\SQLEXPRESS;Database=master;Encrypt=False;Trusted_Connection=True";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordBox.Password;
+
+            if (AuthenticateUser(username, password))
+            {
+                MessageBox.Show("Login successful!");
+
+                // Открывает окно с таблицами
+                TableWindow tableWindow = new TableWindow();
+                tableWindow.Show();
+
+                // Закрывает окно авторизации
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.");
+            }
+        }
+
+        private bool AuthenticateUser(string username, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+
+                int count = (int)command.ExecuteScalar();
+                return count > 0;
+            }
+        }
+
+    }
+}
---
+++ testcafe/Table.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace Wpf_cafe
+{
+    public partial class TableWindow : Window
+    {
+        private const string ConnectionString = "Server=DESKTOP-PGFL9BA\\SQLEXPRESS;Database=master;Encrypt=False;Trusted_Connection=True";
+
+        public TableWindow()
+        {
+            InitializeComponent();
+            LoadFoodData();
+            LoadProductsData();
+        }
+
+        private void LoadFoodData()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                string query = "SELECT * FROM Food";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                foodDataGrid.ItemsSource = dataTable.DefaultView;
+            }
+        }
+
+        private void LoadProductsData()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                string query = "SELECT * FROM Products";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                productsDataGrid.ItemsSource = dataTable.DefaultView;
+            }
+        }
+    }
+}
